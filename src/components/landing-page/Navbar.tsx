c64508080459d8de5c251b5a0ae5afea4accd8df
egileekada <egileoniso.ekada@gmail.com>
2025-09-15 11:08:27 +0100

    Refactor Navbar component to use CustomImage for logo rendering
    - Replaced the Next.js Image component with a CustomImage component for the logo in the Navbar.
    - Updated the logo's container to ensure proper styling and responsiveness.

--- src/components/landing-page/Navbar.tsx
+++ src/components/landing-page/Navbar.tsx
@@ -1,15 +1,24 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { CustomImage } from "../custom";
 
 const Navbar = () => {
   return (
     <nav className="px-[5%] absolute w-full lg:px-[10%] py-8 bg-transparent">
       <div className="flex justify-between items-center 2xl:container mx-auto">
+        {/* <Link href="/">
+          <Image src="/ryzlyLogo.png" alt="ryzlyLogo" width={150} className="w-[150px]" />
+        </Link> */}
         <Link href="/">
-          <Image src="/ryzlyLogo.png" alt="ryzlyLogo" className="w-[150px]" />
-        </Link>
-
+          <div className="w-[150px] h-9 "  >
+            <CustomImage
+              src="/ryzlyLogo.png"
+              alt="ryzlyLogo"
+              fillContainer 
+              className="rounded-full"
+            />
+          </div>
+        </Link> 
         <div>
           <Link
             href=""
