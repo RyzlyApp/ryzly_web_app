45d3fd15db6273ff59be3e0d32b63a5e42d0d1dd
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2025-09-16 15:22:03 +0100

    Fixed the build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Onest } from "next/font/google";
 import "./globals.css";
+import { Provider } from "@/provider";
 
 const onest = Onest({
   variable: "--font-onest",
@@ -12,6 +13,8 @@ export const metadata: Metadata = {
   description: "",
 };
 
+export const dynamic = "force-dynamic";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -19,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={onest.className}>{children}</body>
+      <body className={onest.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
