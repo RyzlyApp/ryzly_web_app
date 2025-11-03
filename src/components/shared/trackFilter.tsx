f197afff765898fcb6900f235a7a11f1dc1b58f4
egileekada <egileoniso.ekada@gmail.com>
2025-11-03 19:49:28 -0300

    refactor(trackFilter): integrate Jotai for filter state management
    - Replaced local state management with Jotai's atom for track filters.
    - Updated button click handlers to modify filter state instead of local selection.
    - Cleaned up imports and improved code readability.

--- src/components/shared/trackFilter.tsx
+++ src/components/shared/trackFilter.tsx
@@ -1,14 +1,16 @@
 import { ITrack } from "@/helper/model/interest"
-import { useFetchData } from "@/hook/useFetchData"
-import { useState } from "react"
+import { useFetchData } from "@/hook/useFetchData" 
 import { CustomButton } from "../custom"
 import LoadingLayout from "./loadingLayout"
+import { useAtom } from "jotai"
+import { filtersAtom } from "@/helper/atom/filter"
 
 export default function TrackFilter() {
 
     const { data: track, isLoading } = useFetchData<ITrack[]>({ endpoint: "/track/tracks", name: "tracks" })
-
-    const [selected, setSelected] = useState<string[]>([])
+ 
+    const [, updateFilters] = useAtom(filtersAtom);
+    const [filters] = useAtom(filtersAtom);
 
     return (
         <LoadingLayout loading={isLoading} >
@@ -16,12 +18,12 @@ export default function TrackFilter() {
                 <div
                     className="flex gap-4 w-fit pb-2"
                 >
-                    <CustomButton onClick={() => setSelected([])} variant={selected?.length > 0 ? "outline" : "primary"} height="35px" fontSize="12px">
+                    <CustomButton onClick={() => updateFilters({...filters, tracks: []})} variant={filters?.tracks?.length > 0 ? "outline" : "primary"} height="35px" fontSize="12px">
                         All
                     </CustomButton>
                     {track?.map((item, index) => {
                         return (
-                            <CustomButton key={index} onClick={() => setSelected([item._id])} variant={item?._id === selected[0] ? "primary" : "outline"} height="35px" fontSize="12px">
+                            <CustomButton key={index} onClick={() => updateFilters({...filters, tracks: [item._id]})} variant={item?._id === filters?.tracks[0] as string ? "primary" : "outline"} height="35px" fontSize="12px">
                                 {item?.name}
                             </CustomButton>
                         )
