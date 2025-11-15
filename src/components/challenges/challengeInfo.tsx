63ba5addcf341ea21205f7690669280845f4f3ee
Neon <danielemmanuel257@gmail.com>
2025-11-16 00:07:16 +0100

    refactor(challengeInfo): remove unused verifyPayment and add missing dependencies
    Add eslint-disable comment for no-explicit-any and include getWallet and wallet in useEffect dependencies

--- src/components/challenges/challengeInfo.tsx
+++ src/components/challenges/challengeInfo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { IChallenge } from "@/helper/model/challenge";
 import { formatNumber } from "@/helper/utils/numberFormat";
 import { CustomButton, CustomImage } from "../custom";
@@ -35,7 +36,7 @@ export default function ChallengeInfo({
   const [creatingOrderLoading, setCreatingOrderLoading] = React.useState(false);
   const [canPay, setCanPay] = React.useState(false);
   const [reference, setReference] = React.useState<string>("");
-  const { wallet, getWallet, createPayment, verifyPayment } =
+  const { wallet, getWallet, createPayment } =
     usePaymentWalletHook();
   const { joinChallenge, isOpen, setIsOpen, endChallenge } = useChallenge(
     item?._id
@@ -47,7 +48,7 @@ export default function ChallengeInfo({
         await getWallet();
       }
     })();
-  }, []);
+  }, [getWallet, wallet]);
 
   const handlePayment = async () => {
     console.log(wallet);
