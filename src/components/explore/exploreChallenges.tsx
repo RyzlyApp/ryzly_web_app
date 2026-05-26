8515f139cd38eb2943609648400566bc27f9f934
egileekada <egileoniso.ekada@gmail.com>
2026-05-26 15:36:35 +0100

    refactor(ExploreChallenges): improve code readability and enhance data fetching parameters
    - Cleaned up import statements and improved formatting for better readability.
    - Enhanced URL parameter handling by adding 'isPublic' to the data fetching parameters.
    - Streamlined the rendering of the LoadingLayout and ChallengeCard components for improved clarity.

--- src/components/explore/exploreChallenges.tsx
+++ src/components/explore/exploreChallenges.tsx
@@ -1,41 +1,49 @@
-import { useFetchData, useUnsecureFetchData } from "@/hook/useFetchData"
-import { ChallengeCard, LoadingLayout } from "../shared"
-import { IChallenge } from "@/helper/model/challenge"
+import { useFetchData, useUnsecureFetchData } from "@/hook/useFetchData";
+import { ChallengeCard, LoadingLayout } from "../shared";
+import { IChallenge } from "@/helper/model/challenge";
 import { filtersAtom } from "@/helper/atom/filter";
 import { useAtom } from "jotai";
 import { searchAtom } from "@/helper/atom/search";
 
 export default function ExploreChallenges() {
-
     const [filters] = useAtom(filtersAtom);
     const [search] = useAtom(searchAtom);
 
-
     const params = new URLSearchParams();
-    params.append('tracks', filters.tracks[0] ?? "");
-    params.append('q', search);
-    params.append('tags', filters.tags[0] ?? "");
-    params.append('type', filters.type ?? "");
-    params.append('participationFee', filters.participationFee?.toString() ?? "");
-    params.append('winningPrice', filters.winningPrice?.toString() ?? "");
-    params.append('Level', filters.level ?? "");
-    params.append('Industry', filters.industry ?? "");
+    params.append("tracks", filters.tracks[0] ?? "");
+    params.append("q", search);
+    params.append("tags", filters.tags[0] ?? "");
+    params.append("type", filters.type ?? "");
+    params.append(
+        "participationFee",
+        filters.participationFee?.toString() ?? "",
+    );
+    params.append("winningPrice", filters.winningPrice?.toString() ?? "");
+    params.append("Level", filters.level ?? "");
+    params.append("Industry", filters.industry ?? "");
 
     const { data, isLoading } = useUnsecureFetchData<IChallenge[]>({
-        endpoint: `/challenge?${params.toString()}`, name: "challenge", params: {
-            isApproved: "true"
-        }
-    })
+        endpoint: `/challenge?${params.toString()}`,
+        name: "challenge",
+        params: {
+            isApproved: "true",
+            isPublic: "true",
+        },
+    });
 
     return (
-        <LoadingLayout loading={isLoading} bgColor={false} lenght={data?.length} >
-            <div className="  max-w-[90%] mx-auto lg:max-w-[80%] w-full grid gap-4 grid-cols-1 lg:grid-cols-3 pb-6 " >
+        <LoadingLayout
+            loading={isLoading}
+            bgColor={false}
+            lenght={data?.length}
+        >
+            <div className="  max-w-[90%] mx-auto lg:max-w-[80%] w-full grid gap-4 grid-cols-1 lg:grid-cols-3 pb-6 ">
                 {data?.map((item, index) => {
                     return (
                         <ChallengeCard explore={true} key={index} data={item} />
-                    )
+                    );
                 })}
             </div>
         </LoadingLayout>
-    )
+    );
 }
