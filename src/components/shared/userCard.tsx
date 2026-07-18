7d15e201087c2433ec5a5ea57c2576e3e328f593
egileekada <egileoniso.ekada@gmail.com>
2026-07-18 11:20:25 +0100

    refactor(UserCard): enhance user type handling and display logic
    - Updated UserCard component to support both IUser and IOrganisationDetails types, improving flexibility.
    - Introduced a type guard for better type checking and clarity in user display logic.
    - Simplified display name logic to ensure accurate representation based on user type, enhancing user experience.

--- src/components/shared/userCard.tsx
+++ src/components/shared/userCard.tsx
@@ -3,34 +3,36 @@ import { IOrganisationDetails, IUser } from "@/helper/model/user";
 import { Avatar } from "@heroui/react";
 import { useRouter } from "next/navigation";
 
+const isUser = (data: IUser | IOrganisationDetails): data is IUser => {
+    return "firstName" in data;
+};
+
 export default function UserCard({
     item,
     showCoach = true,
 }: {
-    item: IUser;
+    item: IUser | IOrganisationDetails;
     showCoach?: boolean;
 }) {
     const router = useRouter();
 
-    // ✅ Type guards
-    // const isUser = (data: IUser | IOrganisationDetails): data is IUser => {
-    //     return "firstName" in data;
-    // };
- 
-    const profilePicture = item?.profilePicture; 
-    
+    const profilePicture = item?.profilePicture;
+    const displayName = isUser(item)
+        ? `${item.firstName ?? ""} ${item.lastName ?? ""}`.trim() || item.fullName
+        : item.name;
+
     return (
         <button
             onClick={() => router.push(`/dashboard/profile/${item?._id}`)}
             className=" flex gap-2 items-center "
         >
-            <Avatar src={profilePicture} name={item?.fullName} />
+            <Avatar src={profilePicture} name={displayName} />
             <div className=" flex flex-col items-start ">
                 <div className=" flex items-center gap-1 ">
                     <p className=" text-sm capitalize font-semibold ">
-                        {item?.firstName+" "+item?.lastName}
+                        {displayName}
                     </p>
-                    {showCoach && item?.isCoach && (
+                    {showCoach && isUser(item) && item?.isCoach && (
                         <div className=" px-2 rounded-full bg-neonblue-600 text-white font-semibold h-[18px] flex justify-center items-center text-xs ">
                             Coach
                         </div>
